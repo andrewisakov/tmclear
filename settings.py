db7b88c51bd5323cab600c5de21a8fab0f3a7f4f
andrewisakov <andrew.isakov@gmail.com>
2017-08-24 17:00:12 +0300

    Подготовка

---
+++ settings.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+DB = dict(database='d:\\tme_db.fdb',
+          host='127.0.0.1',
+          user='sysdba',
+          password='admin',
+          charset='win1251')
+
+MAX_ORDERS = 500
+MIN_days = 100
